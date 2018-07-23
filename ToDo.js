5a17b7b8415c06ef5d2f996f4d7a7fb98d03d97b
JungHyeonWoo <wjdgusdn1223@gmail.com>
2018-07-23 12:55:44 +0900

    todo list making process..

--- ToDo.js
+++ ToDo.js
@@ -1,14 +1,108 @@
 import React, { Component } from "react";
-import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { 
+    View, 
+    Text, 
+    TouchableOpacity, 
+    StyleSheet, 
+    Dimensions 
+} from "react-native";
+
+const { width, height } = Dimensions.get("window");
 
 export default class Todo extends Component{
+    state = {
+        isEditing: false,
+        isCompleted: false
+    }
     render(){
+        const {isCompleted, isEditing} = this.state;
         return(
-            <View>
-                <Text>Hello I'm a To Do</Text>
+            <View style={styles.container}>
+                <View style={styles.column}>
+                    <TouchableOpacity onPress={this._toggleComplete}>
+                        <View 
+                            style={[
+                                styles.circle, 
+                                isCompleted ? styles.completedCircle : styles.uncompletedCircle
+                            ]}
+                        />
+                    </TouchableOpacity>
+                    <Text 
+                        style={[
+                            styles.text, 
+                            isCompleted ? styles.completedText : styles.uncompletedText
+                        ]}
+                    >
+                    Hello I'm a To Do
+                    </Text>
+                </View>
+                <View style={styles.column}>
+                    {isEditing ? (
+                        <View style={styles.actions}>
+                            <TouchableOpacity>
+                                <View style={styles.actionContainer}>
+                                    <Text style={styles.actionText}>✔</Text>
+                                </View>
+                            </TouchableOpacity>
+                        </View>
+                    ) : (
+                        <View style={styles.actions}>
+                            <TouchableOpacity>
+                                <View style={styles.actionContainer}>
+                                    <Text style={styles.actionText}>✎</Text>
+                                </View>
+                            </TouchableOpacity>
+                            <TouchableOpacity>
+                                <View style={styles.actionContainer}>
+                                    <Text style={styles.actionText}>✖</Text>
+                                </View>
+                            </TouchableOpacity>
+                        </View>
+                    )}
+                </View>
             </View>
         )
     }
+    _toggleComplete = () => {
+        this.setState(prevState => {
+            return {
+                isCompleted: !prevState.isCompleted
+            }
+        })
+    }
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    container: {
+        width: width - 50,
+        borderBottomColor: "#bbb",
+        borderBottomWidth: StyleSheet.hairlineWidth,
+        flexDirection: "row",
+        alignItems: "center"
+    },
+    circle: {
+        width: 30,
+        height: 30,
+        borderRadius: 15,
+        borderWidth: 3,
+        marginRight: 20
+    },
+    completedCircle: {
+        borderColor: "#bbb"
+    },
+    uncompletedCircle: {
+        borderColor: "#F23657"
+    },
+    text: {
+        fontWeight: "500",
+        fontSize: 20,
+        marginVertical: 20
+    },
+    completedText: {
+        color: "#bbb",
+        textDecorationLine: "line-through"
+    },
+    uncompletedText: {
+        color: "#353839"
+    }
+});
